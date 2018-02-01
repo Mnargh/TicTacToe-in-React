f596e2531f775ad44ad70612abf41bdd4a6da4e8
Tom Brandon <tom.j.v.brandon@gmail.com>
2018-02-01 12:09:18 +0000

    Changed App to render board

--- src/App.js
+++ src/App.js
@@ -2,12 +2,13 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Cell from './components/Cell.js'
+import Board from './components/Board.js'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <Cell />
+        <Board />
       </div>
     );
   }
