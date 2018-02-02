33e4fe528a9d475684cc6404254215f83e86afe8
Tom Brandon <tom.j.v.brandon@gmail.com>
2018-02-02 00:03:45 +0000

    Spiked code to generate 3 x 3 grid. Will rewrite with test

--- src/components/Board.js
+++ src/components/Board.js
@@ -6,18 +6,36 @@ class Board extends Component {
   constructor(props) {
     super(props);
     this.state = {
-
-    };
+      // render grid
+      board: this.generateGrid(3, 3)
+    }
   }
 
   renderCell(value) {
     return <Cell value={value} />
   };
 
+  generateColumn(columns){
+    var col = []
+    for(var i = 0; i < columns; i++){
+      col.push(this.renderCell(null));
+    }
+    return col;
+  }
+
+  generateGrid(rows, columns) {
+    var grid = []
+
+    for(var j = 0; j < rows; j++){
+        grid.push(this.generateColumn(columns));
+    }
+    return grid;
+  }
+
 
   render() {
     return (
-      <div>{this.renderCell(null)}</div>
+      <div className="board">{this.state.board}</div>
     );
   }
 
