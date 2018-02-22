bb907ddae7d6f99dd1522fcdd4e752f240a2b2c9
Mnargh <tom.j.v.brandon@gmail.com>
2018-02-22 14:12:22 +0000

    Board has empty array for state. Renders 9 Cells

--- src/components/Board.js
+++ src/components/Board.js
@@ -3,44 +3,63 @@ import Cell from './Cell'
 import '../App.css';
 
 class Board extends Component {
+  
   constructor(props) {
     super(props);
     this.state = {
-      
-      // board: this.generateGrid(3, 3)
-      board: Array(3).fill(Array(3).fill(null))
-    }
+      board: Array(9).fill(null)
+    };
   }
 
   
   renderCell(value) {
     return <Cell
-      value={value}
+      value={this.state.board[value]}
       // onClick={() => this.handleClick()}
      />
   };
 
-  generateColumn(columns){
-    var col = []
-    for(var i = 0; i < columns; i++){
-      col.push(this.renderCell(null));
-    }
-    return col;
-  }
+  // generateColumn(columns){
+  //   var col = []
+  //   for(var i = 0; i < columns; i++){
+  //     col.push(this.renderCell(null));
+  //   }
+  //   return col;
+  // }
 
-  generateGrid(rows, columns) {
-    var grid = []
+  // generateGrid(rows, columns) {
+  //   var grid = []
 
-    for(var j = 0; j < rows; j++){
-        grid.push(this.generateColumn(columns));
-    }
-    return grid;
-  }
+  //   for(var j = 0; j < rows; j++){
+  //       grid.push(this.generateColumn(columns));
+  //   }
+  //   return grid;
+  // }
 
 
   render() {
+
+    const status = 'Next player: X;'
+    
     return (
-      <div className="board">{this.state.board}</div>
+      <div>
+        <div className="status">{status}</div>
+          <div className="board-row">
+            {this.renderCell(1)}
+            {this.renderCell(2)}
+            {this.renderCell(3)}
+          </div>
+          <div className="board-row">
+            {this.renderCell(4)}
+            {this.renderCell(5)}
+            {this.renderCell(6)}
+          </div>
+          <div className="board-row">
+            {this.renderCell(7)}
+            {this.renderCell(8)}
+            {this.renderCell(9)}
+          </div>
+      </div>
     );
   }
 
